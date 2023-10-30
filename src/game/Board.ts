9a3e65b0f23d783a55ab53c97b62b1333e43e1a5
Cherryl <developingens@gmail.com>
2023-10-30 20:25:24 +0300

    Move pieces is done

--- src/game/Board.ts
+++ src/game/Board.ts
@@ -1,3 +1,5 @@
+import Notions from "../refs/notions";
+import pieces from "../refs/pieces";
 
 class ChessBoard {
     public pieces: Notion[];
@@ -7,9 +9,30 @@ class ChessBoard {
     }
 
     simulateBoard() {
-        return this.pieces.join(" ")
+        return this.pieces.map(piece => {
+            if (Object.hasOwn(piece, "piece")) {
+                if (piece.letter === "h") {
+                    return piece?.piece?.charAt(0) + "\n"
+                } else return piece?.piece?.charAt(0)
+            } else {
+                if (piece.letter === "h") return "\n"
+            }
+        }).join("")
     }
 
+    movePiece(old_pos: Notion, new_pos: Notion) {
+        console.log(old_pos)
+        let old_index = this.pieces.findIndex(piece => piece.letter + piece.number === old_pos.letter + old_pos.number)
+        let new_index = this.pieces.findIndex(piece => piece.letter + piece.number === new_pos.letter + new_pos.number)
+
+        let old_ = this.pieces[old_index];
+        let new_ = this.pieces[new_index]
+        new_.piece = old_.piece;
+        old_.piece = " ";
+
+        this.pieces[old_index] = old_
+        this.pieces[new_index] = new_;
+    }
 
 }
 
--- src/refs/pieces.ts
+++ src/refs/pieces.ts
@@ -5,21 +5,22 @@ let pieces = notions.map(notion => {
     if (notion.number === 2 || notion.number === 7) {
         notion["piece"] = "Pawn";
     }
-    if ((notion.letter === "a" || notion.letter === "h") && (notion.number === 1 || notion.number === 8)) {
+    else if ((notion.letter === "a" || notion.letter === "h") && (notion.number === 1 || notion.number === 8)) {
         notion["piece"] = "Rook";
     }
-    if ((notion.letter === "b" || notion.letter === "g") && (notion.number === 1 || notion.number === 8)) {
+    else if ((notion.letter === "b" || notion.letter === "g") && (notion.number === 1 || notion.number === 8)) {
         notion["piece"] = "Knight";
     }
-    if ((notion.letter === "c" || notion.letter === "f") && (notion.number === 1 || notion.number === 8)) {
+    else if ((notion.letter === "c" || notion.letter === "f") && (notion.number === 1 || notion.number === 8)) {
         notion["piece"] = "Bishop";
     }
-    if (notion.letter === "d" && (notion.number === 1 || notion.number === 8)) {
+    else if (notion.letter === "d" && (notion.number === 1 || notion.number === 8)) {
         notion["piece"] = "Queen";
     }
-    if (notion.letter === "e" && (notion.number === 1 || notion.number === 8)) {
+    else if (notion.letter === "e" && (notion.number === 1 || notion.number === 8)) {
         notion["piece"] = "Check";
     }
+    else notion["piece"] = " "
     return notion
 });
 
