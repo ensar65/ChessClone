2be436578fa828f3c9811d13a0122e557c2ca63d
Cherryl <developingens@gmail.com>
2023-11-12 18:46:53 +0300

    Config

--- src/refs/simulator.ts
+++ src/refs/simulator.ts
@@ -2,9 +2,10 @@ import deployer from "./deployer";
 
 let notions = deployer();
 
-console.log(notions)
 
 export default function () {
     let pieces = Array.from(notions,([name,value]) => ({name , value}))
-
+    pieces.forEach(data => {
+        console.log(data)
+    })
 }
